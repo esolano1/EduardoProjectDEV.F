ec42ed4efb35c5eaff514a880e6ca9c2159f1871
esolano1 <joeybello23@gmail.com>
2025-02-04 23:00:09 -0600

    Activity of the first lesson of module 3

---
+++ tipos-de-dato.js
@@ -0,0 +1,20 @@
+// This should say it is a number
+console.log(typeof 42);
+// This should say it is a string
+console.log(typeof 'Forty');
+// This should say it is a number
+console.log(typeof -555);
+// This should say it is a boolean
+console.log(typeof true);
+// This should say it is a number
+console.log(typeof 0);
+// This should say it is a number
+console.log(typeof 20);
+// This should say it is a object
+console.log(typeof null);
+// This should say it is a undefined
+console.log(typeof undefined);
+// This should say it is undefined
+console.log(typeof FALSE);
+// This should say it is a undefined
+console.log(typeof TRUE);
