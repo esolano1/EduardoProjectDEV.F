4755157fb320751748c0923ad2f57d2adec4194d
esolano1 <joeybello23@gmail.com>
2025-02-06 21:04:00 -0600

    Tarea de clase 2 - Evaluador de notas

---
+++ Clase-2/evaluador-de-notas.js
@@ -0,0 +1,55 @@
+/*Crea un programa en JavaScript que evalúe la nota de un estudiante y genere un mensaje personalizado basado en la nota. Las notas se asignan de la siguiente manera:
+
+Si la nota es 90 o más, el estudiante aprueba con "Excelente".
+Si la nota está entre 75 y 89, el estudiante aprueba con "Bien".
+Si la nota está entre 60 y 74, el estudiante aprueba con "Suficiente".
+Si la nota es menor de 60, el estudiante no aprueba. */
+
+let nota = 95;
+
+if (nota >= 90) {
+    console.log("Excelente")
+}
+else if (nota > 75 && nota < 89) {
+console.log("Bien")
+}
+else if (nota > 60 && nota < 74) {
+    console.log("Suficiente")
+}
+else {
+    console.log("No aprobado")
+}
+
+// Otro estudiante
+
+let nota2 = 80;
+
+if (nota2 >= 90) {
+    console.log("Excelente")
+}
+else if (nota2 > 75 && nota2 < 89) {
+console.log("Bien")
+}
+else if (nota2 > 60 && nota2 < 74) {
+    console.log("Suficiente")
+}
+else {
+    console.log("No aprobado")
+}
+
+// Otro estudiante
+
+let nota3 = 56;
+
+if (nota3 >= 90) {
+    console.log("Excelente")
+}
+else if (nota3 > 75 && nota3 < 89) {
+console.log("Bien")
+}
+else if (nota3 > 60 && nota3 < 74) {
+    console.log("Suficiente")
+}
+else {
+    console.log("No aprobado")
+}
