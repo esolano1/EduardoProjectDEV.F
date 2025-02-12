a82dff43a2f703d72de98bb443d184d06fd1a248
esolano1 <joeybello23@gmail.com>
2025-02-11 21:26:45 -0600

    Tarea de la clase 3 con el ciclo while

--- Clase-3/clasificacion-de-frutas.js
+++ Clase-3/clasificacion-de-frutas.js
@@ -9,6 +9,9 @@ let uva = 0;
 let pera = 0;
 let platano = 0;
 
+
+//Con ciclo for
+console.log('Numero de frutas con ciclo for');
 for (let i = 0; i < frutas.length ; i++) {
 
     if (frutas[i] === 'guayaba') {
@@ -26,6 +29,43 @@ for (let i = 0; i < frutas.length ; i++) {
     else if (frutas[i] === 'platano') {
         platano += 1;
     }
+}
+
+console.log('guayaba:', guayaba);
+console.log('uva:', uva);
+console.log('pera:', pera);
+console.log('platano:', platano);
+
+
+// Otro método
+
+i=0;
+guayaba = 0;
+uva = 0;
+pera = 0;
+platano = 0;
+console.log('Numero de frutas con ciclo while');
+// Con ciclo while    
+    while (i < frutas.length) {
+
+        if (frutas[i] === 'guayaba') {
+            guayaba += 1;
+        }
+    
+        else if (frutas[i] === 'uva') {
+            uva += 1;
+        }
+    
+        else if (frutas[i] === 'pera') {
+            pera += 1;
+        }
+    
+        else if (frutas[i] === 'platano') {
+            platano += 1;
+        }
+
+i++;
+
 }
 
 console.log('guayaba:', guayaba);
