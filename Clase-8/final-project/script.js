000a048440ebbaff0f0fb192586a6ec181f1b104
esolano1 <joeybello23@gmail.com>
2025-03-04 23:36:17 -0600

    Project almost finished

--- Clase-8/final-project/script.js
+++ Clase-8/final-project/script.js
@@ -4,13 +4,22 @@ let name_input = document.getElementById ("nombre");
 let comments_input = document.getElementById("comentarios");
 let container1 = document.getElementById("contenedor1");
 
-let all_comments_inputs = { comments: [] };
+let all_comments_inputs = { 
+   comments: [] 
+};
+
+/*
+function agregar_comments () {
+   
+}
+*/
 
 button.addEventListener("click", function (event) {
-   let div_element = document.createElement("div");
+   
    let currentDate = new Date();
    let dateTime = currentDate.toLocaleString();
 
+   let div_element = document.createElement("div");
   /* div_element.innerHTML = comments_input.value;*/
   div_element.innerHTML = `<strong>Comentario publicado el:</strong> ${dateTime} <strong>Por:</strong> ${name_input.value} <br>${comments_input.value} <br>`
   container1.className = "muestracomments"; 
@@ -24,4 +33,27 @@ button.addEventListener("click", function (event) {
     container1.removeChild(div_element);
    }
 )
+
+
+all_comments_inputs.comments.push(comments_input.value);
+
+let comments_JSON = JSON.stringify(all_comments_inputs);
+
+localStorage.setItem("commentsAll",comments_JSON)
+
 })
+
+
+/*
+if(localStorage.getItem("commentsAll")) {
+
+   let comments_JSON = localStorage.getItem("commentsAll");
+   commentsAll = JSON.parse(comments_JSON);
+
+   for(let i = 0;i<all_comments_inputs.comments.length;i++) {
+
+
+   }
+
+}
+   */
