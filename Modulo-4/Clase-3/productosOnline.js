fa5b9e5d39acb7628b82abd0cce8b4b735844f4f
EduardoSolano <joeybello23@gmail.com>
2025-03-25 21:52:20 -0600

    adding template of products

---
+++ Modulo-4/Clase-3/productosOnline.js
@@ -0,0 +1,9 @@
+//arreglo de objetos con al menos 5 productos, cada uno con las propiedades nombre, precio y categoría.
+
+const productos = [
+    { nombre: "Camiseta", precio: 15, categoria: "Ropa" },
+    { nombre: "Laptop", precio: 800, categoria: "Electrónica" },
+    { nombre: "Libro", precio: 12, categoria: "Educación" },
+    { nombre: "Zapatos", precio: 50, categoria: "Ropa" },
+    { nombre: "Celular", precio: 600, categoria: "Electrónica" },
+];
