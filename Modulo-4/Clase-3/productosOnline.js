fadec15a7d23a436c0fdb65c8dfaf34523bccac8
EduardoSolano <joeybello23@gmail.com>
2025-03-26 00:42:05 -0600

    Tarea de clase 3

--- Modulo-4/Clase-3/productosOnline.js
+++ Modulo-4/Clase-3/productosOnline.js
@@ -7,3 +7,24 @@ const productos = [
     { nombre: "Zapatos", precio: 50, categoria: "Ropa" },
     { nombre: "Celular", precio: 600, categoria: "Electrónica" },
 ];
+
+//Productos menos de 100 pesos
+const menosDeCien = productos.filter(producto => producto.precio < 100);
+console.log(menosDeCien);
+
+//Ordenados alfabéticamente
+productos.sort((a, b) => {
+    if (a.nombre < b.nombre) return -1; // Si a.nombre es menor, 'a' va primero
+    if (a.nombre > b.nombre) return 1;  // Si a.nombre es mayor, 'b' va primero
+    return 0; // Si son iguales, no se cambia el orden
+  });
+
+console.log(productos);
+
+//Nuevo arreglo únicamente para los nombres de cada producto
+const nombresProductos = productos.map(producto => producto.nombre);
+console.log(nombresProductos);
+
+//Al menos un producto arriba de 100
+const masDeCien = productos.filter(producto => producto.precio > 100);
+console.log(masDeCien);
